7854a63960eff8d9ed060feb510c09743499edae
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2026-01-21 23:03:42 -0300

    projeto utilizado

---
+++ DevFreela-repository/DevFreela.Application/ApplicationModule.cs
@@ -0,0 +1,24 @@
+﻿using DevFreela.Application.Commands.InsertProject;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevFreela.Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services
+                .AddHandlers();
+
+            return services;
+        }
+
+        private static IServiceCollection AddHandlers(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+                config.RegisterServicesFromAssemblyContaining<InsertProjectCommand>());
+
+            return services;
+        }
+    }
+}
